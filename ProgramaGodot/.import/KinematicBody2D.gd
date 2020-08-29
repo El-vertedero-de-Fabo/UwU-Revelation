3cfeff1c71ab49d5e70e1b7eda2cf3608960a200
Fabo xw <faboGG.12@gmail.com>
2020-08-29 01:06:20 -0300

    MovimientosDePersonaje

--- ProgramaGodot/.import/KinematicBody2D.gd
+++ ProgramaGodot/.import/KinematicBody2D.gd
@@ -1,17 +1,22 @@
 extends KinematicBody2D
 
-var motion = Vector2()
 
+var movimiento = Vector2()
+const salto = Vector2(0,-1)
 
-func _physics_process(delta):
-	
+func _physics_process(_delta):
+	movimiento.y +=10  #gravedad del objeto	
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x=100
-		
+		movimiento.x=100
 	elif Input.is_action_pressed("ui_left"):
-		motion.x=-100
-	else :
-		motion.x = 0
-	move_and_slide(motion)
+		movimiento.x=-100
+	else:
+		movimiento.x = 0
+	if is_on_floor():
+		print("On floor.")
+		if Input.is_action_just_pressed("ui_up"):
+			movimiento.y = -400
+	
+	move_and_slide(movimiento,salto)
 	pass
